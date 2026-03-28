495bdff1cc5cf94fdcc3284d853dee1e8cab8bda
Charlie Groves <ctrg04@gmail.com>
2026-03-28 23:42:35 +0000

    better looking map

--- frontend/src/PropertyMap.tsx
+++ frontend/src/PropertyMap.tsx
@@ -40,8 +40,10 @@ export default function PropertyMap({
   return (
     <MapContainer center={center} zoom={zoom} style={{ height: '100%', width: '100%' }}>
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+        subdomains="abcd"
+        maxZoom={19}
       />
       {properties
         .filter((p) => p.latitude != null && p.longitude != null)
